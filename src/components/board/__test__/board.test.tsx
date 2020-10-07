77dc35be138684804ad09c86192d5e6e62b04c16
Claudio Ricci <claudioricci1@hotmail.com>
2020-10-07 01:38:46 +0100

    extra App and Board tests

--- src/components/board/__test__/board.test.tsx
+++ src/components/board/__test__/board.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Board from "..";
+import { H1Tag } from "../../typography";
 import renderer from "react-test-renderer";
+import "jest-styled-components";
 
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
@@ -13,5 +15,18 @@ describe("Board Tests", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Board />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+});
+
+describe("Title Tests", () => {
+  it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<H1Tag></H1Tag>, div);
+  });
+
+  test("H1 Renders Correctly", () => {
+    const tree = renderer.create(<H1Tag />).toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule("color", "#D9819D");
   });
 });
